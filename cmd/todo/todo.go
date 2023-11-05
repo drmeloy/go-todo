e69f3d614b2083ed49a429fcf8d2c49c32f1510e
unknown <drmeloy@gmail.com>
2023-11-05 09:02:28 -0800

    writing methods

--- cmd/todo/todo.go
+++ cmd/todo/todo.go
@@ -1,6 +1,12 @@
 package todo
 
-import "time"
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"os"
+	"time"
+)
 
 type item struct {
 	Task      string
@@ -18,4 +24,50 @@ func (t *Todos) Add(task string) {
 		CreatedAt: time.Now(),
 		CompletedAt: time.Time{},
 	}
+
+	*t = append(*t, todo)
+}
+
+func (t *Todos) Complete(index int) error {
+	ls := *t
+
+	if index <= 0 || index > len(ls) {
+		return errors.New("invalid index")
+	}
+
+	ls[index - 1].CompletedAt = time.Now()
+	ls[index - 1].Done = true
+	return nil
+}
+
+func (t *Todos) Delete(index int) error {
+	ls := *t
+
+	if index <= 0 || index > len(ls) {
+		return errors.New("invalid index")
+	}
+
+	*t = append(ls[:index - 1], ls[index:]...)
+	return nil
+}
+
+func (t *Todos) Load(filename string) error {
+	file, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+
+	if len(file) ==  0 {
+		return err
+	}
+	err = json.Unmarshal(file, t)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
